85c3eb9b9741115fb905114ed5347a80ab58481a
3la2i <alaa.ata25@gmail.com>
2024-06-04 01:44:34 +0300

    solved tasks almost

---
+++ questions.js
@@ -0,0 +1,41 @@
+var x = -5;
+var y = 3;
+var z = x * y;
+console.log(z);
+
+var result = "JavaScript" + 50;
+console.log(result);
+
+var x = 17;
+var y = 5;
+var z = x % y;
+var e = y % x;
+console.log(e);
+console.log(z);
+
+var x = 5;
+var y = 10;
+var z = x / y;
+console.log(z);
+
+var result = 4 === "4";
+console.log(result);
+
+var result = 7 <= 8;
+console.log(result);
+
+var result = "Hello" + 5;
+console.log(result);
+
+var x = 5;
+var result = Math.pow(x, 2);
+console.log(result);
+
+
+// question2
+
+var number = prompt("Please enter a number:");
+
+number = number;
+
+alert("You entered: " + number);
